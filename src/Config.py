92880affcff5746863764cb56fbb246e25b55d70
r9kawai <r9kawai@gmail.com>
2023-09-22 17:05:30 +0100

    tune_20230923_2

--- src/Config.py
+++ src/Config.py
@@ -54,8 +54,8 @@ class Configuration:
         self.ps4_torot_color = PS4_TOROT_COLOR
 
         #################### COMMANDS ####################
-        self.max_x_velocity = 0.3                           # ok amend
-        self.max_y_velocity = 0.2                           # ok amend
+        self.max_x_velocity = 0.28                          # ok amend
+        self.max_y_velocity = 0.18                          # ok amend
         self.max_yaw_rate = 1.25                            # ok amend
         self.max_pitch = 25.0 * np.pi / 180.0               # ok amend
 
@@ -79,7 +79,7 @@ class Configuration:
 
         #################### SWING ######################
         self.z_coeffs = None
-        self.z_clearance = 0.08                             # ok amend
+        self.z_clearance = 0.075                            # ok amend
         self.alpha = (
             0.5  # Ratio between touchdown distance and total horizontal stance movement
         )
@@ -95,7 +95,7 @@ class Configuration:
             [[1, 1, 1, 0], [1, 0, 1, 1], [1, 0, 1, 1], [1, 1, 1, 0]]
         )
         self.overlap_time = (
-            0.18  # duration of the phase where all four feet are on the ground     # ok amend
+            0.19  # duration of the phase where all four feet are on the ground     # ok amend
         )
         self.swing_time = (
             0.12  # duration of the phase when only two feet are on the ground      # ok amend
