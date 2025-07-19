986eaa62e9eeae894e1c4f4a320180b406635cd9
r9kawai <r9kawai@gmail.com>
2025-07-19 10:24:39 +0100

    v0.2_dev_0719a

--- 2_boot_service_entry.sh
+++ 2_boot_service_entry.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo Entry robot.service and joystick.service ...
+sudo rm /etc/systemd/system/joystick.service
 sudo ln -s /home/pi/PrintPupper/src/PupperCommand/joystick.service /etc/systemd/system/joystick.service
+sudo rm /etc/systemd/system/robot.service
 sudo ln -s /home/pi/PrintPupper/src/robot.service /etc/systemd/system/robot.service
 systemctl daemon-reload
 systemctl enable joystick.service
