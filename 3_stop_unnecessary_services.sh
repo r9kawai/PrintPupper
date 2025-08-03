5262174eb2078a1b2088a08a938434e6af0e0ac7
r9kawai <r9kawai@gmail.com>
2025-08-03 17:54:56 +0900

    0803b_as_soft_setting_script

--- 3_stop_unnecessary_services.sh
+++ 3_stop_unnecessary_services.sh
@@ -1,29 +1,41 @@
 #!/bin/sh
-echo Stop unnecessary services.
-systemctl stop alsa-restore.service
-systemctl disable alsa-restore.service
-systemctl stop cron.service
-systemctl disable cron.service
-systemctl stop dphys-swapfile.service
-systemctl disable dphys-swapfile.service
-systemctl stop triggerhappy.service
-systemctl disable triggerhappy.service
-systemctl stop avahi-daemon.service
-systemctl disable avahi-daemon.service
-systemctl stop console-setup.service
-systemctl disable console-setup.service
-systemctl stop rsyslog.service
-systemctl disable rsyslog.service
-systemctl stop ModemManager.service
-systemctl disable ModemManager.service
-systemctl stop keyboard-setup.service
-systemctl disable keyboard-setup.service
-systemctl stop rpi-eeprom-update.service
-systemctl disable rpi-eeprom-update.service
-systemctl stop systemd-random-seed.service
-systemctl disable systemd-random-seed.service
-systemctl stop systemd-networkd-wait-online.service
-systemctl mask systemd-networkd-wait-online.service
-sudo sed -i -e '$atimeout 15\n' /etc/dhcpcd.conf
-echo ...done, Please reboot
+echo "Stop unnecessary services..."
+sudo systemctl disable avahi-daemon
+sudo systemctl disable hciuart
+sudo systemctl disable triggerhappy
+sudo systemctl disable man-db.timer
+sudo systemctl disable apt-daily.timer
+sudo systemctl disable apt-daily-upgrade.timer
+sudo systemctl mask apt-daily.service
+sudo systemctl mask apt-daily-upgrade.service
+sleep 1
 
+echo "Disable swap."
+sudo swapoff --all
+sudo systemctl stop dphys-swapfile
+sudo systemctl disable dphys-swapfile
+sleep 1
+
+echo "/var/log is on ramdisk."
+sudo sed -i '/\/var\/log/ d; $a tmpfs /var/log tmpfs defaults,noatime,nosuid,size=20m,mode=0755 0 0' /etc/fstab
+sleep 1
+
+echo "Disable IPv6."
+sudo sed -i -e '/^net.ipv6.conf.all.disable_ipv6/d' \
+            -e '/^net.ipv6.conf.default.disable_ipv6/d' \
+            -e '$a net.ipv6.conf.all.disable_ipv6 = 1\nnet.ipv6.conf.default.disable_ipv6 = 1' \
+            /etc/sysctl.conf
+sleep 1
+
+echo "Network timeout is short. (5sec)"
+sudo nmcli connection modify preconfigured ipv4.dhcp-timeout 5
+sudo nmcli connection modify preconfigured connection.wait-device-timeout 5000
+sudo nmcli connection show preconfigured | grep ipv4.dhcp-timeout
+sudo nmcli connection show preconfigured | grep connection.wait-device-timeout
+sleep 1
+
+echo "...done."
+echo "After 5sec, reboot..."
+sleep 5
+echo "Start reboot."
+sudo reboot
