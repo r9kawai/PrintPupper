b293f39f3eb6aedb7c5538c1954817dc1465ac7c
r9kawai <r9kawai@gmail.com>
2025-08-12 21:47:52 +0900

    0812_as_soft_fixInstallScript

--- 1_Install_robot_depend_packages.sh
+++ 1_Install_robot_depend_packages.sh
@@ -5,9 +5,10 @@ sudo apt install libatlas-base-dev -y
 sudo apt install python3-dev -y
 sudo apt install python3-pip -y
 sudo apt install python3-pigpio -y
-sudo apt install python3-numpy -y
+sudo pip install numpy
 sudo pip install serial
 sudo pip install UDPComms
 sudo pip install ds4drv
 sudo pip install transforms3d
+sudo pip install evdev==1.4.0
 echo "...done"
